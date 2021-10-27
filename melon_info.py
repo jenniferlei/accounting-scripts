7256f6565775c88f6af5489b4c4a41e642a080b9
Jennifer Lei <jenniferlei1928@gmail.com>
2021-10-27 21:34:51 +0000

    Updated melon data on melons.py and removed functions to convert into dict and add attributes on melon_info.py

--- melon_info.py
+++ melon_info.py
@@ -4,28 +4,6 @@
 from melons import melon_names, melon_seedlessness, melon_prices
 
 
-def melon_dict(names, seedless, prices):
-    '''convert current melon data into dictionary'''
-    melon_dict = {}
-    for key in melon_names:
-        melon_dict[names[key]] = {'price': prices[key], 'seedless': seedless[key]}
-    return melon_dict
-
-melon_dict = melon_dict(melon_names, melon_seedlessness, melon_prices)
-print(melon_dict)
-
-def add_attribute_to_dict(current_dict, new_attributes):
-    '''adds new list of attributes with values None to an existing dictionary for each key'''
-    for each_item in current_dict:
-        for attribute in new_attributes:
-            current_dict[each_item][attribute] = None
-    return current_dict
-
-new_attr = ['flesh_color', 'rind_color', 'weight']
-new_melon_dict = add_attribute_to_dict(melon_dict, new_attr)
-print(new_melon_dict)
-
-
 def print_melon(name, seedless, price):
     """Print each melon with corresponding attribute information."""
 
